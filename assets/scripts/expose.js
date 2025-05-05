3a0222c394596a3061bb51940180d009673510ea
AiMurphy-UCSD <aimurphy@ucsd.edu>
2025-05-05 13:58:35 -0700

    Added/Edited package.json and README.dm , Began work on expose.js (image and audio selection)

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,32 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  // Attach an EventListener to the horn selector
+  const hornSelector = document.querySelector('#horn-select');
+
+  hornSelector.addEventListener('change', function() {
+    // This code is ran when the there is change to which horn is selected
+
+    // Get the individual horn selected from the selector
+    const selectedHorn = hornSelector.value;
+
+    // Depending on the horn selected, change the image and audio source
+    switch (selectedHorn) { 
+      case 'air-horn':
+        document.querySelector('img').src = 'assets/images/air-horn.svg';
+        document.querySelector('audio').src = 'assets/audio/air-horn.mp3';
+        break;
+      case 'car-horn':
+        document.querySelector('img').src = 'assets/images/car-horn.svg';
+        document.querySelector('audio').src = 'assets/audio/car-horn.mp3';
+        break;
+      case 'party-horn':
+        document.querySelector('img').src = 'assets/images/party-horn.svg';
+        document.querySelector('audio').src = 'assets/audio/party-horn.mp3';
+        break;
+      default:
+        console.log('No horn selected');    
+    }
+  })
 }
