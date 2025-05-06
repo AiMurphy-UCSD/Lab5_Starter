8b728f18f99a5918de893dfa66563d1af5967b9a
AiMurphy-UCSD <aimurphy@ucsd.edu>
2025-05-06 14:52:52 -0700

    Unit Testing

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 
 Aidan Murphy
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+I would not make a unit test for the "message" feature as there are many components that makes up a message and it is quite broad. This unit test would not test simply one 'unit', it would test the message feature itself, network communication between users, and backend logic. Unit tests are best for small isolated units of code, and this feature is neither small nor isolated.
+
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+I would use a unit test to test the "max message length" feature of a messaging application, because creating the test itself would be quite simple, but it would also keep the specifications of the program in line. In a messaging application, the message itself is the key part of the application, so you want to make sure everything is taken care of in regards to that feature. Max message length is a well defined component and can easily be tested.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,85 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// isPhoneNumber
+// isPhoneNumber tests true
+test('valid phone number', () => {
+  expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+test('valid phone number with dashes', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+// isPhoneNumber tests false
+test('invalid phone number with letters', () => {
+  expect(isPhoneNumber('123-abc-7890')).toBe(false);
+});
+test('invalid phone number empty', () => {
+  expect(isPhoneNumber('')).toBe(false);
+});
+
+
+// isEmail
+// isEmail tests true
+test('valid email', () => {
+  expect(isEmail('adogmurphy@gmail.com')).toBe(true);
+} );
+test('valid email with numbers', () => {
+  expect(isEmail('aimurphy1234@ucsd.edu')).toBe(true);
+} );
+// isEmail tests false
+test('invalid email with no domain', () => {
+  expect(isEmail('adog.murphy@')).toBe(false);
+} );
+test('invalid email with no username', () => {
+  expect(isEmail('@gmail.com')).toBe(false);
+} );
+
+// isStrongPassword
+// isStrongPassword tests true
+test('valid strong password', () => {
+  expect(isStrongPassword('a1b2c3d4')).toBe(true);
+} );
+test('valid strong password with underscore', () => {
+  expect(isStrongPassword('a1b2_c3d4')).toBe(true);
+} );
+// isStrongPassword tests false
+test('invalid strong password with special characters', () => {
+  expect(isStrongPassword('a1b2c3d4!')).toBe(false);
+} );
+test('invalid strong password with spaces', () => {
+  expect(isStrongPassword('a1b2 c3d4')).toBe(false);
+} );
+
+// isDate
+// isDate tests true
+test('valid date', () => {
+  expect(isDate('12/31/2023')).toBe(true);
+} );
+test('valid date with 1 digit month and day', () => {
+  expect(isDate('1/1/2023')).toBe(true);
+} );
+// isDate tests false
+test('invalid date with letters', () => {
+  expect(isDate('12/31/2023a')).toBe(false);
+} );
+test('invalid date with special characters', () => {
+  expect(isDate('12/31/2023!')).toBe(false);
+} );
+
+// isHexColor
+// isHexColor tests true
+test('valid hex color', () => {
+  expect(isHexColor('#123456')).toBe(true);
+} );
+test('valid hex color with 3 digits', () => {
+  expect(isHexColor('#123')).toBe(true);
+} );
+// isHexColor tests false
+test('invalid hex color with invalid letters', () => {
+  expect(isHexColor('#123zvx')).toBe(false);
+} );
+test('invalid hex color with special characters', () => {
+  expect(isHexColor('#123!')).toBe(false);
+} );
+
